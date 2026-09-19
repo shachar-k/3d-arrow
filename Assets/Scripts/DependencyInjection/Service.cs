f1027efa34fea2fdd7f7d6c5a81dc0c40b61e9f4
shachk <shachk@example.com>
2026-09-19 11:21:43 +0300

    move files

--- Assets/Scripts/DependencyInjection/Service.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class Containerble<T> : Singleton<Containerble<T>> where T : MonoBehaviour
-{
-    #region Name
-        private IContainerService Container => ContainerService.Instance;
-    #endregion
-}
---
+++ Assets/Scripts/Managers/GameManager/GameSettings.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameSettings", menuName = "Scriptable Objects/GameSettings")]
+public class GameSettings : ScriptableObject
+{
+    
+}
