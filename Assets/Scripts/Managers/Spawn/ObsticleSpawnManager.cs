5857a639c4f463293ddd96a071495a6324074724
shachk <shachk@example.com>
2026-09-22 16:06:43 +0300

    add spawner

---
+++ Assets/Scripts/Managers/Spawn/ObsticleSpawnManager.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class ObsticleSpawnManager : Injectable<ObsticleSpawnManager,IObsticleSpawnManager>, IObsticleSpawnManager
+{
+    [SerializeField]
+    private List<GameObject> objectsToSpawn = new List<GameObject>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
